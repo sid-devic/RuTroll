fc7936d8c9759a3c74fd661853a88753cec88d7e
Jamie <jlt10@rice.edu>
2018-10-20 22:54:34 -0400

    Begin working on API request

--- chromeExtension/myScript.js
+++ chromeExtension/myScript.js
@@ -1,3 +1,28 @@
+const endpoint = "https://automl.googleapis.com/v1beta1/projects/tidy-arcade-220013/locations/us-central1/models/TCN5448917125241496931:predict"
+const payload = {
+    "payload" : {
+        "textSnippet": {
+           "content": "I love playing with my cat!",
+            "mime_type": "text/plain"
+        },
+    }
+}
+
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+        console.log(request.responseText);
+    }
+    else {
+        console.log("Failed: "+request.responseText);
+    }
+}
+
+request.open('POST', endpoint, true);
+request.send(JSON.stringify(payload));
+
 var tweets = document.getElementsByClassName('js-tweet-text tweet-text');
 for (var i = 0, l = tweets.length; i < l; i++) {
     tweets[i].innerText = 'Some text';
