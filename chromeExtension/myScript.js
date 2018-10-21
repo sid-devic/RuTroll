19f1aa19d635995381182cf033a555b38464e301
Jamie <jlt10@rice.edu>
2018-10-21 01:57:47 -0400

    Got the API request to AutoML functional

--- chromeExtension/myScript.js
+++ chromeExtension/myScript.js
@@ -1,29 +1,38 @@
-const endpoint = "https://automl.googleapis.com/v1beta1/projects/tidy-arcade-220013/locations/us-central1/models/TCN5448917125241496931:predict"
-const payload = {
-    "payload" : {
-        "textSnippet": {
-           "content": "I love playing with my cat!",
-            "mime_type": "text/plain"
-        },
-    }
-}
 
 
-var request = new XMLHttpRequest();
+{/* <script src="bundle.js"></script> */}
+{/* <script type="text/javascript" src="auth.json"></script> */}
+// $.getJSON("auth.json", function(json) {
+//     console.log(json); // this will show the info it in firebug console
+// });
 
-request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-        console.log(request.responseText);
-    }
-    else {
-        console.log("Failed: "+request.responseText);
-    }
-}
 
-request.open('POST', endpoint, true);
-request.send(JSON.stringify(payload));
+
+// const scopes = 'https://www.googleapis.com/auth/cloud-platform'
+// const jwt = new google.auth.JWT(key.client_email, null, key.private_key, scopes);
+
 
 var tweets = document.getElementsByClassName('js-tweet-text tweet-text');
 for (var i = 0, l = tweets.length; i < l; i++) {
     tweets[i].innerText = 'Some text';
 }
+
+const endpoint = "https://automl.googleapis.com/v1beta1/projects/tidy-arcade-220013/locations/us-central1/models/TCN2618236018595798875:predict"
+fetch(endpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+        "payload" : {
+          "textSnippet": {
+               "content": "I love playing with my cat!",
+                "mime_type": "text/plain"
+           },
+        }
+      }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "Authorization": "Bearer ya29.c.Elo9BhsVNqmCwRGlEvx81RXtVrJa73jCcV3ggLzX44nXdkvvOWFIJhrQKrAl4gwRdVhgkiUambxlwtad0XRX_ekw_nlVb4qC1yxnxDev56ntBRBVUQVdcjT8swo"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+
